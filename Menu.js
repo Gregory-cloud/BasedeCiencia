d658e9bbed003af8dfd52b073b0db38847ad7555
Gregory-cloud <70454783+Gregory-cloud@users.noreply.github.com>
2020-09-11 16:08:20 -0400

    Add files via upload

---
+++ Menu.js
@@ -0,0 +1,42 @@
+// $(document).ready(main);
+
+// var contador = 1;
+
+// function main() {
+//     $('.menu_bar').click(function () {
+//         //$('nav').toggle();
+
+//         if (contador == 1) {
+//             $('nav').animate({
+//                 left: '0'
+//             });
+//             contador = 0;
+//         } else {
+//             contador = 1;
+//             $('nav').animate({
+//                 left: '-100%'
+//             });
+//         }
+//     });
+// }
+
+// $(document).ready(function () {
+//     var altura = $('.menu').offset().top;
+
+//     $(window).on('scroll', function () {
+//         if ($(window).scrollTop() > altura) {
+//             $('.menu').addClass('menu-fixed');
+//         } else {
+//             $('.menu').removeClass('menu-fixed');
+//         }
+//     });
+// });
+
+// document.getElementById('boton').onclick = function () {
+//     document.getElementById("iconos-sociales").classList.toggle("desaparecer");
+// }
+
+// // var botonEncima = document.querySelector("#boton");
+
+// // botonEncima.addEventListener("mouseout", function(){alert("Sígueme en mis redes sociales, anda, no seas así...")});
+
